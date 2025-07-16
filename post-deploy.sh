61e38a655534cde89f28e01fcf86f101b548acd4
INJY <178879836+IvenNJY@users.noreply.github.com>
2025-07-16 17:32:35 +0800

    Delete post-deploy.sh

--- post-deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script runs after deployment to optimize Laravel
-# It should be called via a webhook or manually after deployment
-
-# Note: Since Vercel functions are stateless, we can't cache config/routes
-# So we'll configure Laravel to work without caching in production
-
-echo "Laravel optimization skipped - using stateless configuration for Vercel"
